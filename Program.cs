9f43f7b00a12dd4f9c30a5f52eaf94dfadc22e63
sandiks <sannddik@gmail.com>
2020-01-10 19:23:24 +0300

    clean file

--- Program.cs
+++ Program.cs
@@ -10,15 +10,14 @@ namespace Console.Codewars
         {
             var data = new int[] { -6, -4, -3, -2, 0, 1, 2, 3, 6, 8 };
             TransformList(data);
-            //Console.WriteLine("Hello World!");
         }
 
         private static void TransformList(int[] data)
         {
             var res = new List<List<int>>(10);
-            res.Add(new List<int>(10){data[0]});
+            res.Add(new List<int>(10) { data[0] });
 
-            for (int i = 0; i < data.Length-1; i++)
+            for (int i = 0; i < data.Length - 1; i++)
             {
                 var curr = data[i + 1];
                 if (curr - data[i] == 1)
@@ -27,13 +26,13 @@ namespace Console.Codewars
                 }
                 else
                 {
-                    res.Add(new List<int>(10){curr});
+                    res.Add(new List<int>(10) { curr });
                 }
             }
 
             foreach (var span in res)
             {
-                var SpanInfo = span.Count()>2 ? $"{span.First()}-{span.Last()}" : String.Join(",",span); 
+                var SpanInfo = span.Count() > 2 ? $"{span.First()}-{span.Last()}" : String.Join(",", span);
                 System.Console.WriteLine(SpanInfo);
             }
         }
